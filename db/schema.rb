68b5b6803fac1e56ddcbc8266b830b84fa139c93
MrDSunshine <p14161601@my365.dmu.ac.uk>
2017-03-22 01:12:07 +0000

    Create posts table, model and validation

--- db/schema.rb
+++ db/schema.rb
@@ -11,6 +11,13 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 0) do
+ActiveRecord::Schema.define(version: 20170322005759) do
+
+  create_table "posts", force: :cascade do |t|
+    t.string   "title"
+    t.text     "description"
+    t.datetime "created_at"
+    t.datetime "updated_at"
+  end
 
 end
